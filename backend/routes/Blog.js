2e668849a4a9b13746e8d01648ced0f0edda18a6
Jahid Hasan Jitu <jahidjitu095@gmail.com>
2025-11-14 23:42:01 +0600

    updated

--- backend/routes/Blog.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { Create } from '../controllers/Blog.js';
-
-
-const BlogRoutes = express.Router();
-
-BlogRoutes.post("/create", Create)
-
-export default BlogRoutes;
--- backend/utils/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await mongoose
-    .connect(process.env.MONGODB_URL)
-    .then(() => console.log("DB Connected"));
-};
